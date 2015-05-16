ec46d3abf06e02117cc2e6248782ba66050229c3
zhangbo127 <1224680008@qq.com>
2015-05-17 07:45:12 +0800

    添加自定义的knockout underscore模块

---
+++ app/js/lib/knockout/knockout-custom.js
@@ -0,0 +1,4 @@
+define(['knockout-default'], function(ko){
+	window.ko = ko;
+	return ko;
+});
---
+++ app/js/lib/underscore/underscore-custom.js
@@ -0,0 +1,5 @@
+define(['underscore-default', 'underscore.string'], function (_, _str) {
+	window._ = _;
+	_.string = _str;
+	return _;
+});
